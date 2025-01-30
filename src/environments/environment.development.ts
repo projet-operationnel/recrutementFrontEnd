1e68601f9aa11ee7566da71d123761578c87be1f
FadalNdiaye1011 <fndiaye1011@gmail.com>
2025-01-30 16:41:16 +0000

    changement api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   url : {
-      api : "https://recrutementtuteutunchk-v2.onrender.com/api/v1/",
+      api : "https://recrutementtuteutunchk-v3.onrender.com/api/v1/",
   },
   appName : "gestionRecrutement",
   linkPhoto:'http://localhost:8000/storage/images/',
